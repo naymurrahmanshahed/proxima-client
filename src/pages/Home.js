f1b5e9d76bafcc253c31d6d947bd3f8c0146e033
naymurrahmanshahed <naymurrahmanshahed@gmail.com>
2023-03-02 19:22:48 +0600

    done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,11 +11,14 @@ const Home = () => {
   const { user } = useAuthContext();
   useEffect(() => {
     const getAllProject = async () => {
-      const res = await fetch("http://localhost:5000/api/projects", {
-        headers: {
-          Authorization: `Bearer ${user.token}`,
-        },
-      });
+      const res = await fetch(
+        `${process.env.REACT_APP_BASE_URL}/api/projects`,
+        {
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+          },
+        }
+      );
       const json = await res.json();
 
       if (res.ok) {
@@ -28,14 +31,14 @@ const Home = () => {
   }, [dispatch, user]);
   return (
     <div className="home container mx-auto py-10 xl:py-20 grid grid-cols-2 xl:grid-cols-3 gap-10">
-      <div className="left col-span-1 xl:col-span-2">
-        {projects.length > 0 ? (
-          <ProjectTitle ProjectTitle={"All Projects"} />
-        ) : (
-          <p className="flex items-center justify-center translate-y-1/2">
+      <div className="left  col-span-1 xl:col-span-2">
+        {projects?.length < 1 ? (
+          <p className="flex flex-col items-center justify-center ">
             No Project added
           </p>
-        )}
+        ) : null}
+
+        {projects?.length > 0 && <ProjectTitle ProjectTitle={"All Projects"} />}
 
         <div className="projects-wrapper flex gap-10 flex-wrap">
           {projects &&
